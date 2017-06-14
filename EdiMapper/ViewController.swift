b969f846755eaa96b9c71f562743ee8140c4a946
Laptop <home@MacAir.local>
2017-06-14 18:11:04 -0400

    First commit

--- EdiMapper/ViewController.swift
+++ EdiMapper/ViewController.swift
@@ -12,16 +12,11 @@ class ViewController: NSViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        start()
     }
 
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
+    func start() {
+        //
     }
-
-
 }
 
